3d0320b389b6383211393b416314ee366c051752
MBEKE <ogadambeke@gmail.com>
2024-03-19 01:14:26 +0300

    new-file: 6-print_matrix_integer.py
    the function prints a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,9 @@
+def print_matrix_interger(matrix=[[]]):
+    """Print a matrix of integers."""
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
+
+    if not matrix:
+        print()
