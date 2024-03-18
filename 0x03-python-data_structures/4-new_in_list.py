b7ae4f590cbfb6c1675649f00f86fb36bdc6c8f3
MBEKE <ogadambeke@gmail.com>
2024-03-19 00:56:06 +0300

    new-file: 4-new_in_list.py
    The function replaces an element at specific position without modifyng
    the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+def new_in_list(miy_list, idx, element):
+    """Replace an element in a list at a specific position without modifying
+    the orignal list"""
+    if idx < 0 or idx >= len(miy_list):
+        return miy_list.copy()
+
+    new_list = miy_list.copy()
+    new_list[idx] = element
+    return new_list
