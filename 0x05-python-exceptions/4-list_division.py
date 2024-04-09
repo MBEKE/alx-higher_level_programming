8d5706374b745a7bc536caa8627ee7b11f6cfb28
MBEKE <ogadambeke@gmail.com>
2024-04-09 15:45:50 +0300

    fix: handle case of non-float/int element

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,16 +4,25 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            division = my_list_1[i] / my_list_2[i]
-        except ZeroDivisionError:
-            print("division by 0")
-            division = 0
-        except TypeError:
-            print("Wrong type")
-            division = 0
+            if i >= len(my_list_1) or i >= len(my_list_2):
+                print("out of range")
+                result.append(0)
+            elif my_list_2[i] == 0:
+                print("division by 0")
+                result.append(0)
+            elif not all(isinstance(x, (int, float)) for x in [my_list_1[i], my_list_2[i]]):
+                print("wrong type")
+                result.append(0)
+            else:
+                division = my_list_1[i] / my_list_2[i]
+                result.append(division)
         except IndexError:
             print("out of range")
-            division = 0
-        finally:
-            result.append(division)
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
     return result
