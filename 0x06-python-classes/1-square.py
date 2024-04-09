ffc6e674dabb48fa517b2c6e77087eaf49c96a76
MBEKE <ogadambeke@gmail.com>
2024-04-10 00:59:38 +0300

    feat: Implement Square class with private size atr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module defines a class Square that represents a square
+"""
+
+class Square:
+    """
+    A class that defines a square.
+
+    Atrributes:
+        __size (int): The size of the square
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square Instance.
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
