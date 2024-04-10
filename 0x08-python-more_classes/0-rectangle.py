eac3866356745437b7e75e63fc5a5ffa71ff19d4
MBEKE <ogadambeke@gmail.com>
2024-04-10 18:32:22 +0300

    feat: implement class Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class Rectangle that defines a rectacle
+"""
+
+
+class Rectangle:
+    """
+    Empty class that defines a rectacle
+    """
+    pass
